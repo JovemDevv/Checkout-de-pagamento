33162168029f6e232c7bbc45eda7bc5caff496a0
Ana Caroline Vasconcellos <101136752+FFinalBoss@users.noreply.github.com>
2022-06-15 20:57:49 -0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- 
+# (use a # para comentar no seu script) Na linha anterior, estamos declarando a codificação, porque python tem origem da lingua inglesa, então declaramos a codificação para que os acentos venham ser aceitos. 
+print("Hello world!")
+print("Olá, mundo!")
+
+"""
+Quando você quiser fazer um comentario com mais de uma 
+linha
+de
+código
+"""
