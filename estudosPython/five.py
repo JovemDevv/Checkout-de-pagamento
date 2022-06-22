3eef289b5f20b498f4a7138a7e4a0b1238d7a3f4
Ana Caroline Vasconcellos <101136752+FFinalBoss@users.noreply.github.com>
2022-06-22 12:12:05 -0300

    Add files via upload

---
+++ estudosPython/five.py
@@ -0,0 +1,35 @@
+# Estruturas condicionais
+
+a = 4
+b = 34667890
+
+if a > b:
+    print("a é maior que y")
+
+if b > a:
+    print("b é maior que a")
+
+# else
+
+x = 5
+y = 10
+
+if x> y:
+    print("x maior que y")
+
+else:
+    print("x não é maior que y")
+
+# elif
+
+x = 1
+y = 2
+
+if x == y:
+    print("nueros iguais")
+elif y > x:
+    print("y maior que x")
+elif x<y:
+    print("x menor que y")
+else:
+    print("numeros diferentes")
---
+++ estudosPython/three.py
@@ -0,0 +1,12 @@
+# Operadores relacionais
+x = 2
+y = 3
+
+print(x == y)
+
+x = 10
+y = 11
+
+soma = x + y
+
+print(soma > y)
