7612403297165f0ad16594277c069e2bbb81c8f0
Ana Caroline Vasconcellos <101136752+JovemDevv@users.noreply.github.com>
2022-09-28 00:30:16 -0300

    Add files via upload

---
+++ Evolução/1variaveis.py
@@ -0,0 +1,10 @@
+#variavel
+
+nome = "Ana"
+idade = "28"
+
+print("Conheço uma mulher chamada " + nome + ",")
+print("Ela tem " + idade + " anos.")
+
+''''tipos de variaveis:
+ string = é um texto simples. Ela é sempre entre aspas'''
