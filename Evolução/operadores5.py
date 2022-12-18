66cb6bd51991711c853866058ded70f0abebd426
JovemDevv <anacarolinesan18@gmail.com>
2022-12-18 15:15:27 -0300

    Operadores de atribuição

---
+++ Evolução/operadores5.py
@@ -0,0 +1,14 @@
+""""Operadores de associação(in)"""
+
+curso = "curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+print("Python" in curso)
+#True
+
+print("maçã" not in frutas)
+#True
+
+print(200 in saques)
+#False
