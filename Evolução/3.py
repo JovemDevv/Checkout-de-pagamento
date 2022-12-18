228323bc63390e67e4ec68a6a9f74f06fb2b7033
JovemDevv <anacarolinesan18@gmail.com>
2022-12-17 23:08:35 -0300

    operadores

---
+++ Evolução/3.py
@@ -0,0 +1,45 @@
+#Operadores aritméticos
+
+#adição
+print(1 + 1)
+
+#subtração
+print(10 - 2)
+
+#multiplicação
+print(4 * 3)
+
+#divisão
+print(12 / 3)
+
+#divisão inteira
+print(12 // 2)
+
+#módulo
+print(10 % 3)
+
+#exponenciação
+print(2 ** 3)
+
+"""A ordem que o python irá seguir para fazer a operação:
+- parentesis
+- expoentes
+- multiplicações e divisões(da esquerda para a direita)
+- somas e subtrações(da esquerda para a direita)"""
+#exemplos:
+
+print((10 - 5) * 2)
+
+print(10 ** (2 *2))
+
+print(10 / 2 * 4)
+
+#Exemplos na pratica
+
+idade_1= 13
+
+idade_2= 19
+
+print(idade_1 + idade_2)
+print(idade_1 // idade_2)
+print(idade_1 ** idade_2)
