428e108ba6d515de6b6ba81bf48e3f8f75230040
Ana Caroline Vasconcellos <101136752+JovemDevv@users.noreply.github.com>
2025-01-22 22:25:47 -0300

    Delete Evolução directory

--- Evolução/conversor-segundos.py
+++
@@ -1,9 +0,0 @@
-segundos = int(input("Por favor, entre com o número de segundos que deseja converter: "))
-dias = segundos // 86400
-segundos = segundos % 86400
-horas = segundos // 3600
-segundos = segundos % 3600
-minutos = segundos // 60
-segundos = segundos % 60
-print("%d dias, %d horas, %d minutos e %d segundos." % (dias, horas, minutos, segundos))
-
--- Evolução/digito-dezenas.py
+++
@@ -1,3 +0,0 @@
-numero = int(input("Digite um número inteiro: "))
-dezenas = (numero // 10) % 10
-print("O dígito das dezenas é", dezenas)
